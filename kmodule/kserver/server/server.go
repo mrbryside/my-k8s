67cf28c9d6f58a117fcd0dff71137a3a9576ebcf
Bank Sirawat <sirawat.i@linecorp.com>
2025-04-15 01:35:45 +0700

    feat: remove unused comment move yaml file

--- kmodule/kserver/server/server.go
+++ kmodule/kserver/server/server.go
@@ -8,8 +8,6 @@ import (
 	"github.com/mrbryside/ketcd"
 )
 
-// test
-
 // EtcdClient interface defines the methods needed from the etcd client
 type EtcdClient interface {
 	ListPods(ctx context.Context, namespace string) ([]ketcd.PodInfo, error)
